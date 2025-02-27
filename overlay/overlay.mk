50a184140674683b25158c1e90cc2a16abb95a3d
Sean Du <do4suki@gmail.com>
2025-02-27 14:26:01 +0800

    overlay: Use QTI common QSSI overlays
    Signed-off-by: Sean Du <do4suki@gmail.com>

---
+++ overlay/overlay.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    FrameworksResCommon \
+    OPlusWifiResCommon \
+    SettingsProviderResCommon \
+    SystemUIResCommon \
+    WifiResCommon
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
